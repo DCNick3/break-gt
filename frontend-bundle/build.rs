0789e730d51d555559db8162c007ba94b8d15212
Nikita Strygin <nikita6@bk.ru>
2022-03-03 21:49:58 +0300

    Automagically install dependencies when building front

--- frontend-bundle/build.rs
+++ frontend-bundle/build.rs
@@ -5,11 +5,25 @@ fn main() {
     let frontend_path =
         Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("../frontend");
 
-    Command::new("yarn")
-        .arg("build")
-        .current_dir(&frontend_path)
-        .status()
-        .unwrap();
+    assert_eq!(
+        Command::new("yarn")
+            .current_dir(&frontend_path)
+            .status()
+            .unwrap()
+            .code()
+            .unwrap(),
+        0,
+    );
+    assert_eq!(
+        Command::new("yarn")
+            .arg("build")
+            .current_dir(&frontend_path)
+            .status()
+            .unwrap()
+            .code()
+            .unwrap(),
+        0,
+    );
 
     // rerun in any directory besides dist has changed
     for ref name in std::fs::read_dir(frontend_path)
@@ -18,7 +32,7 @@ fn main() {
         .filter(|f| f.file_name() != "dist" && f.file_name() != "node_modules")
         .map(|f| f.path().to_str().unwrap().to_string())
     {
-        // println!("cargo:warning=dicks {}", name);
+        //println!("cargo:warning=dicks {}", name);
         println!("cargo:rerun-if-changed={}", name);
     }
 }
