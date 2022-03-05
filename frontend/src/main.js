b1c2682d57548dec5fb75054164f562bb34fc952
Nikita Strygin <nikita6@bk.ru>
2022-03-05 21:44:35 +0300

    Use NODE_ENV to determine api url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,13 @@ import "./index.css";
 import VueSSE from "vue-sse";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8081/";
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "/api/"
+    : "http://localhost:8081/api/";
+console.log("Api URL is " + API_URL);
+
+axios.defaults.baseURL = API_URL;
 
 createApp(App)
   .use(VueSSE, {
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
+const webpack = require('webpack')
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        API_URL: JSON.stringify(process.env === 'production' ? '/api/' : 'http://localhost:8081/api/'),
+      }),
+    ]
+  },
 });
